c2f831b9a557db065e19cbd753e24ad33f7a4eae
Adel Orobets <orobetsadel@gmail.com>
2025-06-12 15:51:21 +0300

    Created class PropertiesReader, file config.properties - use in class HomePage [url], file test_data.properties - use in test [testUserCanLoginWithPropertiesData]

---
+++ src/main/java/utils/PropertiesReader.java
@@ -0,0 +1,25 @@
+package utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertiesReader {
+
+    public static String getProperty(String fileName, String key) {
+        Properties properties = new Properties();
+        String filePath = "src/test/resources/properties" + File.separator + fileName;
+
+        try (FileInputStream fileInputStream = new FileInputStream(filePath)) {
+            properties.load(fileInputStream);
+            return properties.getProperty(key);
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to read property '" + key + "' from file: " + filePath, e);
+        }
+    }
+
+    public static String getConfigProperty(String key) {
+        return getProperty("config.properties", key);
+    }
+}
